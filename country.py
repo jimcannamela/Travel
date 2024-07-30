3a46a621bfa56aa362d32aaf56e63fb13af6a88f
Jim Cannamela <jim.cannamela@outlook.com>
2024-07-30 14:35:51 -0500

    coded calls to countriesnow

---
+++ country.py
@@ -0,0 +1,109 @@
+####  GET COUNTRY DATA 
+
+# This module makes all of the calls to the countries now API
+# to retrieve information about a country
+
+import requests
+
+
+# Retrieves a list of all countries for validation.
+# TODO: determine how to build a list box with this data
+
+def get_all_countries():
+    
+    url = "https://countriesnow.space/api/v0.1/countries/iso"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    result = response.json()
+
+    # print(response.text)
+
+    return result
+
+# Retrieves a list of all of the cities for a country
+
+def get_cities(country):
+
+    url = "https://countriesnow.space/api/v0.1/countries/cities"
+
+    payload = {
+        "country": country
+    }
+
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    result = response.json()
+
+    return result
+
+# Retrieves the curency for a country
+
+def get_currency(iso2):
+
+    url = "https://countriesnow.space/api/v0.1/countries/currency"
+
+    payload = {
+        "iso2": iso2
+    }
+        
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    result = response.json()
+
+    return result
+
+
+# Retreives the flag url for a country
+
+def get_flag(iso2):
+
+    url = "https://countriesnow.space/api/v0.1/countries/flag/images"
+
+    payload = {
+        "iso2": iso2
+    }
+        
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    result = response.json()
+
+    return result
+
+# Retreives the longitude and latitude for a country
+# TODO: need to investigate if the other API is at the city level
+
+def get_position():
+
+    url = "https://countriesnow.space/api/v0.1/countries/positions"
+
+    payload = {
+        "iso2": iso2
+    }
+        
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    result = response.json()
+
+    return result
+
+### End of Module ###
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #  Travel Dashboard
+import country
 
 # ASSUMPTION: User is in the United States and are travelling to a foreign country
 '''
@@ -33,3 +34,58 @@ need to find an api which would have airline, car rental and hotel information
 need to find an api which has things to do information
 
 '''
+
+# Get country data for list box
+
+countries = country.get_all_countries()
+
+# TODO: need to handle when error is True
+
+print(type(countries))
+# print(countries)
+print(countries['error'])
+print(countries['msg'])
+
+print("Welcome to Travel Dashboard!")
+
+# travel_country = input("Please enter which country you would like to visit: ")
+
+# print(travel_country)
+
+# travel_city = input("Please enter which city in this country you would like to visit: ")
+
+# print(travel_city)
+
+travel_country='Spain'
+country_iso2=''
+travel_city='Lisbon'
+
+for country in countries['data']:
+    if country['name'] == travel_country:
+        country_iso2=country['Iso2']
+        break
+
+print(f"Country name: {travel_country} iso2: {country_iso2}")
+
+
+# is travel_country in country data
+
+
+
+travel_country='Timbucktooey'
+travel_city='ThreePO'
+
+
+# is travel_country in country data
+country_iso2=''
+for country in countries['data']:
+    if country['name'] == travel_country:
+        country_iso2=country['Iso2']
+        break
+
+print(f"Country name: {travel_country} iso2: {country_iso2}")
+
+if country_iso2 == '':
+    print('Country not found, please try a different country')
+
+    
