97b33f55d7a810e5e4716cb300c9a72adfd4ddd3
Jim Cannamela <jim.cannamela@outlook.com>
2024-08-01 14:28:06 -0500

    got cascading listboxes to work without javascript

--- app.py
+++ app.py
@@ -8,26 +8,20 @@ default_currency='USD'
 country_data = get_countries_and_cities()
 countries=[country['country'] for country in country_data['data']] # list compehension
 
-
 # TODO: Check 'error' and 'msg' to ensure there were no problems
 
 @app.route('/',methods=['GET', 'POST'])
 def index():    
-    return render_template('index.html', countries=countries)
+    selected_country = request.form.get('country', 'Afghanistan').strip() 
+    cities = country_data['data'][countries.index(selected_country)]['cities']
+    return render_template('index.html', countries=countries, selected_country=selected_country, cities=cities)
 
-###### How do i capture and save the cooresponding iso2 code?
-###### How do i capture and save the city data?
-
-@app.route('/get_cities', methods=['GET'])
-def get_cities():
-    country = request.args.get('country')
-    print("I got this country back: ", country)
-    cities = country_data['data'][countries.index(country)]['cities']
-    print(cities)
-    country_iso2 = country_data['data'][countries.index(country)]['iso2']
-    print(country_iso2)
-    return cities
-    # return None
+# @app.route('/get_cities', methods=['GET'])
+# def get_cities():
+#     country = request.args.get('country')
+#     cities = country_data['data'][countries.index(country)]['cities']
+#     country_iso2 = country_data['data'][countries.index(country)]['iso2']
+#     return cities
 
 if __name__ == "__main__":
     app.run()
--- app2.py
+++ app2.py
@@ -1,47 +1,26 @@
-# First attempt at getting flask to work, displays the countries correctly, 
-# when city is entered and submit button clicked error occurs
-
-# discovered a different API which returns the country, iso codes, and cities in one request
-# going to try with that API to determine if that would work better.
-
-from flask import Flask, render_template, request, redirect, url_for
-from country import get_all_countries, get_cities
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from country import get_countries_and_cities
 
 # Create the Flask app and to instantiate flask function as app
 app = Flask(__name__)
 
 default_currency='USD'
+country_data = get_countries_and_cities()
+countries=[country['country'] for country in country_data['data']] # list compehension
+
 
 # TODO: Check 'error' and 'msg' to ensure there were no problems
 
 @app.route('/',methods=['GET', 'POST'])
-def index():
-    print('hello')
-    country_data = get_all_countries()
-    print(country_data['data'][0])
-    countries=[country['name'] for country in country_data['data']] # list compehension
-    city_data=None
-    print(request.method)
-    if request.method == 'POST':
-        country = request.form['country']
-        print(country)
-        city = request.form['city']
-        print(city)
-        city_data=get_cities(country)
-        # city_data=[city_name for city_name in city_data['data'] if city_name['city'] == city] # list comprehension
-        if 'data' in city_data and city in city_data['data']:
-            city_data={'city':city, 'country':country}
-    return render_template('index.html', countries=countries, city_data=city_data)
+def index():    
+    return render_template('index.html', countries=countries)
 
-# def index():
-#     country_list=[]
-#     iso2_list=[]
-#     for cntry in country_data['data']:
-#         country_list.append(cntry['name'])
-#         iso2_list.append(cntry['Iso2'])
-#     return render_template('index.html', country_list=country_list)
-
-
+@app.route('/get_cities', methods=['GET'])
+def get_cities():
+    country = request.args.get('country')
+    cities = country_data['data'][countries.index(country)]['cities']
+    country_iso2 = country_data['data'][countries.index(country)]['iso2']
+    return cities
 
 if __name__ == "__main__":
     app.run()
